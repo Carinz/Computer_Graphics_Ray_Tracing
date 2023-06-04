9ba298609994fb887d3deff461c68cce681076d8
Carinz <flaxercarina@gmail.com>
2023-06-04 11:13:35 +0300

    t fixes

--- ray_tracer.py
+++ ray_tracer.py
@@ -1,6 +1,7 @@
 import argparse
 from PIL import Image
 import numpy as np
+import math
 
 from camera import Camera
 from light import Light
@@ -46,43 +47,113 @@ def parse_scene_file(file_path):
                 raise ValueError("Unknown object type: {}".format(obj_type))
     return camera, scene_settings, objects
 
+def seperate_objects(objects):
+    materials = list(filter(lambda x: isinstance(x, Material) ,objects))
+    planes = list(filter(lambda x: isinstance(x, InfinitePlane) ,objects))
+    cubes = list(filter(lambda x: isinstance(x, Cube) ,objects))
+    spheres = list(filter(lambda x: isinstance(x, Sphere) ,objects))
+    lights = list(filter(lambda x: isinstance(x, Light) ,objects))
+
+    return materials, planes, cubes, spheres, lights
+
+def calc_screen_parameters(camera: Camera, screen_ratio):
+    screen_vec_forward = np.array(camera.look_at)
+    screen_vec_forward = screen_vec_forward / np.linalg.norm(screen_vec_forward)
+
+    screen_vec_w = np.cross(camera.look_at, camera.up_vector)
+    screen_vec_w = screen_vec_w / np.linalg.norm(screen_vec_w)
+    screen_vec_w = screen_vec_w * camera.screen_width
+
+    screen_vec_h = np.cross(screen_vec_w, camera.look_at)
+    screen_vec_h = screen_vec_h / np.linalg.norm(screen_vec_h)
+    screen_vec_h = screen_vec_h * camera.screen_width * screen_ratio
+    
+    screen_center = np.array(camera.position) + screen_vec_forward * camera.screen_distance
+    screen_top_left = screen_center - 0.5*screen_vec_w - 0.5*screen_vec_h
+    return screen_top_left, screen_vec_w, screen_vec_h
+
+def get_pixel_coordinates(row, col, top_left, screen_vec_w, screen_vec_h, width, height):
+    pixel_coords = top_left + (col/width)*screen_vec_w + (row/height)*screen_vec_h
+    return pixel_coords
 
 
 def render_scene(camera: Camera, scene_settings, objects, width, height):
-    materials, surfaces, lights = seperate_objects(objects)
+    camera_position = np.array(camera.position)
+    screen_ratio = height/width
+    materials, planes, cubes, spheres, lights = seperate_objects(objects)
+    screen_top_left, screen_vec_w, screen_vec_h = calc_screen_parameters(camera, screen_ratio)
+    for row in range(height):
+        for col in range(width):
+            pixel_coords = get_pixel_coordinates(row, col, screen_top_left, screen_vec_w, screen_vec_h, width, height)
+            direction = (pixel_coords-camera_position)/np.linalg.norm(pixel_coords-camera_position)
+            color = render_ray(camera_position, direction, scene_settings, materials, planes, cubes, spheres, lights)
 
-    for row in height:
-        for col in width:
-            pixel_coords = get_pixel_coordinates(row, col, height, width, camera)
-            color = render_ray(camera.position, pixel_coords, scene_settings, materials, surfaces, lights)
 
+def calc_color(start,direction,surfaces):
 
+def render_ray(start, direction, scene_settings, materials, planes, cubes, spheres, lights):
 
-def render_ray(start, direction, scene_settings, materials, surfaces, lights):
+    intersections = calc_intersections(start, direction, planes, cubes, spheres)# list of tuples: (object,[ts])
+    if len(intersections)==0:
+        #TODO: color = background color
+        pass
 
-    intersections = calc_intersections(start, direction, surfaces)
-    # if None return background
-    nearest_surface = find_nearest(start, intersections)
+    nearest_surface = find_nearest(start, intersections) # t and surface
 
     intersection_point = calc_inter_point(start, direction, nearest_surface)
 
     compute_lights(direction, lights)
     
-    t_start, t_end = get_transparency_vector()
-    transparency_color =  render_ray()
+    next_start = get_transparency_vector()
+    transparency_color =  render_ray(next_start,direction)
     
     r_start, r_end = get_reflection_vector()
     transparency_color =  render_ray()
 
-def plane_intersect_point(plane : InfinitePlane, start, direction_vec):
+def calc_intersections(start, direction, planes, cubes, spheres):
+    intersect_surfaces=[]
+
+    for sphere in spheres:
+        t_s += calc_sphere_intersections(start, direction, sphere)
+
+    for plane in planes:
+        t_s += plane_intersect_t(plane, start, direction)
+
+    for cube in cubes:
+        t_s += cube_intersect_ts(cube, start, direction)
+    return t_s
+
+
+def calc_sphere_intersections(start, direction, sphere : Sphere):
+    a = np.dot(direction, direction)
+    b = 2 * np.dot(direction, np.subtract(start, sphere.position))
+    c = np.dot(np.subtract(start, sphere.position), np.subtract(start, sphere.position)) - sphere.radius ** 2
+
+    discriminant = b**2 - 4*a*c
+
+    if discriminant < 0:
+        return []
+    elif discriminant == 0:
+        t = -b / (2 * a)
+        intersection_point = start + direction*t
+        return [intersection_point]
+    else:
+        # Two intersections
+        t1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        t2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        intersection_point1 = start + direction*t1
+        intersection_point2 = start + direction*t2
+        return [intersection_point1, intersection_point2]
+
+def plane_intersect_t(plane : InfinitePlane, start, direction_vec): #returns list of t's
     #t = -(P0 • N - d) / (V • N)
     if np.dot(direction_vec,plane.normal) == 0:
-        return None
+        return []
     t = - (np.dot(start,plane.normal)-plane.offset)/(np.dot(direction_vec,plane.normal))
-    intersect_point = start + t*direction_vec
-    return intersect_point
+    #intersect_point = start + t*direction_vec
+    return [t]
 
-def cube_intersect(cube : Cube, start, direction_vec):
+def cube_intersect_ts(cube : Cube, start, direction): #returns list of t's
     x_axis = np.array([1,0,0])
     y_axis = np.array([0,1,0])
     z_axis = np.array([0,0,1])
@@ -105,24 +176,43 @@ def cube_intersect(cube : Cube, start, direction_vec):
     far_plane = InfinitePlane(x_axis,np.dot(x_axis,far_point))
 
     cube_planes = [up_plane,down_plane,left_plane,right_plane,near_plane,far_plane]
-    intersection_points = [plane_intersect_point(p,start,direction_vec) for p in cube_planes]
-    intersection_points = [i for i in intersection_points if i is not None]
+    intersection_ts=[]
+    for plane in cube_planes:
+        intersection_ts+=plane_intersect_t(plane,start,direction)
 
-    if len(intersection_points)==0:
-        return None 
+    if len(intersection_ts)==0:
+        return [] 
 
-    intersection_points = np.unique(np.array(intersection_points))
+    unique_ts=[]
+    for t in intersection_ts:
+        if not any(np.isclose(t, t_val) for t_val in unique_ts): #TODO check that works fine
+            unique_ts.append(t)
+    #unique_ts=np.array(unique_ts)
+
+    intersection_points = [start+t*direction for t in unique_ts]
+
+    #intersection_points = np.unique(np.array(intersection_points))
+    # unique_points=[]
+    # for point in intersection_points:
+    #     if not any(np.allclose(point, selected_vector) for selected_vector in unique_points):
+    #         unique_points.append(point)
+
+    # intersection_points = np.array(unique_points)
 
     intersection_points[ (z_p-offset<intersection_points[:,2]) & (intersection_points[:,2]<z_p+offset) ]
     intersection_points[ (y_p-offset<intersection_points[:,1]) & (intersection_points[:,1]<y_p+offset) ]
     intersection_points[ (x_p-offset<intersection_points[:,0]) & (intersection_points[:,0]<x_p+offset) ]
 
     if len(intersection_points)==0:
-        return None 
+        return []
 
-    norms = np.linalg.norm(intersection_points - start, axis=1)
-    closest_point = intersection_points[np.argmin(norms)]
-    return closest_point
+    intersection_ts=[(point-start)/np.linalg.norm(point-start) for point in intersection_points]
+
+    return intersection_ts
+
+    #norms = np.linalg.norm(intersection_points - start, axis=1)
+    #closest_point = intersection_points[np.argmin(norms)]
+    #return closest_point
 
 def save_image(image_array):
     image = Image.fromarray(np.uint8(image_array))
@@ -133,7 +223,7 @@ def save_image(image_array):
 
 def main():
     parser = argparse.ArgumentParser(description='Python Ray Tracer')
-    parser.add_argument('--scene_file', type=str, default='scenes/pool.txt', help='Path to the scene file')
+    parser.add_argument('--scene_file', type=str, default='scenes/test.txt', help='Path to the scene file') #TODO change to pool
     parser.add_argument('--output_image', type=str, default='output/test.png', help='Name of the output image file')
     parser.add_argument('--width', type=int, default=500, help='Image width')
     parser.add_argument('--height', type=int, default=500, help='Image height')
@@ -142,7 +232,7 @@ def main():
     # Parse the scene file
     camera, scene_settings, objects = parse_scene_file(args.scene_file)
 
-    render_scene(camera, scene_settings, objects, args.width, args.heigth)
+    render_scene(camera, scene_settings, objects, args.width, args.height)
 
     # Dummy result
     image_array = np.zeros((500, 500, 3))
